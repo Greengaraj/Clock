f75a3f72d471c61cf70436b7c64b5da014ac7068
Greengaraj <114313542+Greengaraj@users.noreply.github.com>
2022-11-29 20:09:57 +0300

    Update Settings.h

--- Settings.h
+++ Settings.h
@@ -1,8 +1,8 @@
-// пины к которым подключены кнопки
+// Pins to which the buttons are connected
 #define BUTTON_PIN_1 0
 #define BUTTON_PIN_2 1
 #define BUTTON_PIN_3 4
 #define BUTTON_PIN_4 5
 
-RTC clock;                          // Создаю объект для работы с часами реального времени
-LiquidCrystal_I2C lcd(0x3F, 16, 2); // Создаю объект-экран, передаю используемый адрес и разрешение экрана
+RTC clock;                          // Creating an object for working with a real-time clock
+LiquidCrystal_I2C lcd(0x3F, 16, 2); // Creating a screen object, passing the address used and the screen resolution
